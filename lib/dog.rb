7cba8a02bcfccfdd45415426cb9904d15b078ac8
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-24 01:49:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,11 +2,16 @@ class Dog
   def name (name)
     @name= name
   end
-  def name
-    @name 
-  end
+  
+  #def name
+   # @name 
+ # end
+  
   def name=(new_name)
     @name= new_name
   end
+  
+  def breed()
+  
 end
 
